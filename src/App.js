bb3a3a4b4f5251757cd58470fef2610d465dac75
vaishnavi0003 <93784195+vaishnavi0003@users.noreply.github.com>
2023-08-18 17:03:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+
+import Products from "./Products";
+import Contact from "./Contact";
+import SingleProduct from "./SingleProduct";
+import Cart from "./Cart";
+import ErrorPage from "./ErrorPage";
+import { GlobalStyle } from "./GlobalStyle";
+import { ThemeProvider } from "styled-components";
+
+const App = () => {
+  const theme = {
+    colors : {
+      bg : "#000",
+    },
+  };
+
+
+  return (
+    <ThemeProvider theme = {theme}>
+      <Router>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element = {<Home />} />
+          <Route path="/about" element = {<About />} />
+          <Route path="/products" element = {<Products />} />
+          <Route path="/contact" element = {<Contact />} />
+          <Route path="/singleproduct/:id" element = {<SingleProduct />} />
+          <Route path="/cart" element = {<Cart />} />
+          <Route path="*" element = {<ErrorPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  )
+};
+
+export default App;
